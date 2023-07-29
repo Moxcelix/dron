10bbb312de28c7e53fad36848f6dfe1420cb65fd
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-07-29 21:44:04 +0300

    add simple dron controls

--- Assets/Scripts/Core/Dron/Agregates/Propeller.cs
+++ Assets/Scripts/Core/Dron/Agregates/Propeller.cs
@@ -4,28 +4,28 @@ namespace Core.Dron
 {
     public class Propeller
     {
-        private readonly float _power;
+        private readonly float _maxPower;
         private readonly Vector3 _liftForce;
         private readonly Vector3 _sideForce;
 
-        public float RPM { get; set; }
-        public float Range { get; set; }
+        public float PowerRange { get; set; }
+        public float TurnRange { get; set; }
         public Vector3 Force { get; private set; }
 
         public Propeller(float power, Vector3 liftPower, Vector3 sidePower)
         {
-            _power = power;
+            _maxPower = power;
             _liftForce = liftPower.normalized;
             _sideForce = sidePower.normalized;
         }
 
         public void Update()
         {
-            var range = Mathf.Clamp(Range, -1.0f, 1.0f);
+            var range = Mathf.Clamp(TurnRange, -1.0f, 1.0f);
             var liftForce = _liftForce * Mathf.Sin(Mathf.Acos(range));
             var sideForce = _sideForce * range;
 
-            Force = RPM * _power * (liftForce + sideForce);
+            Force = PowerRange * _maxPower * (liftForce + sideForce);
         }
     }
 }
--- Assets/Scripts/Core/Dron/Dron.cs
+++ Assets/Scripts/Core/Dron/Dron.cs
@@ -9,6 +9,22 @@ namespace Core.Dron
             _propellers = propellers;
         }
 
+        public void Power(float range)
+        {
+            foreach(var propeller in _propellers)
+            {
+                propeller.PowerRange = range;
+            }
+        }
+
+        public void Turn(float range)
+        {
+            foreach (var propeller in _propellers)
+            {
+                propeller.TurnRange = range;
+            }
+        }
+
         public void Update()
         {
             foreach (var propeller in _propellers) 
