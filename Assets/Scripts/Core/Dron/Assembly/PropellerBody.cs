a743da7381dff3345390c39c16a76972a38b4225
Moxcelix <121300999+Moxcelix@users.noreply.github.com>
2023-07-31 15:20:30 +0300

    add propeller body

---
+++ Assets/Scripts/Core/Dron/Assembly/PropellerBody.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Core.Dron
+{
+    public class PropellerBody : MonoBehaviour
+    {
+        [SerializeField] private float _speed = 1.0f;
+        [SerializeField] private Transform _blades;
+
+        private Propeller _propeller;
+
+        public void Apply(Propeller propeller)
+        {
+            _propeller = propeller;
+        }
+
+        private void FixedUpdate()
+        {
+            var velocity = Time.fixedDeltaTime * _propeller.Force * _speed;
+
+            transform.localEulerAngles += Vector3.up * velocity;
+        }
+    }
+}
